0343275d17d3228b6367363385c4348cd084be13
hovablack <nevermind92@hotmail.com>
2020-11-27 19:16:28 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', NULL, 'M', NULL, NULL, NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', NULL, 'M', NULL, NULL, NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', NULL, 'F', NULL, NULL, NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', NULL, 'M', NULL, NULL, NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', NULL, 'F', NULL, NULL, NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', NULL, 'M', NULL, NULL, NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', NULL, 'F', NULL, NULL, NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, 'F', NULL, NULL, NULL);
